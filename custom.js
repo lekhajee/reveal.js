28af0072e2c976ddc6d072035ba0978e95f2120b
Glyph <glyph@twistedmatrix.com>
2014-10-27 12:44:15 -0700

    generalize video-playing logic for other demos

--- custom.js
+++ custom.js
@@ -6,10 +6,18 @@ function inIframe () {
     }
 }
 
-document.addEventListener('first-demo', function() {
+document.addEventListener('slidechanged', function(event) {
     if (!inIframe()) {  // speaker notes
-        var aVideo = document.getElementById("first-demo-video");
-        aVideo.currentTime = 0.0;
-        aVideo.play();
+        var sectionTag = Reveal.getCurrentSlide();
+        var videos = document.getElementsByTagName("video");
+        for (var i = 0; i < videos.length; i++) {
+            var eachVideo = videos[i];
+            if (eachVideo.parentNode === sectionTag) {
+                console.log("starting a video");
+                console.log(eachVideo);
+                eachVideo.currentTime = 0.0;
+                eachVideo.play();
+            }
+        }
     }
-} );
+});
