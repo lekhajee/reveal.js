46f7a333e44683b0f92513024d1491b0d46ffc53
Glyph <glyph@twistedmatrix.com>
2014-11-03 22:45:18 +0100

    play videos in "current" speaker-notes frame, but not in "next"

--- custom.js
+++ custom.js
@@ -1,13 +1,18 @@
-function inIframe () {
-    try {
-        return window !== window.parent;
-    } catch (e) {
-        return true;
+
+function isPreviewFrame() {
+    var frameElement = window.frameElement;
+
+    if (frameElement !== null) {
+        if (frameElement.getAttribute("id") === "next-slide") {
+            return true;
+        }
     }
+    return false;
+
 }
 
-document.addEventListener('slidechanged', function(event) {
-    if (!inIframe()) {  // speaker notes
+Reveal.addEventListener('slidechanged', function(event) {
+    if (!isPreviewFrame()) {
         var sectionTag = Reveal.getCurrentSlide();
         var videos = document.getElementsByTagName("video");
         for (var i = 0; i < videos.length; i++) {
