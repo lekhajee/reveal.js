c9c0e6291a68c813e7311b4aaa4f5fd045e34500
lekhajee <lekha.jeevan@rackspace.com>
2015-02-23 23:57:07 -0600

    workshop slides

---
+++ custom.js
@@ -0,0 +1,26 @@
+
+function isPreviewFrame() {
+    var frameElement = window.frameElement;
+
+    if (frameElement !== null) {
+        if (frameElement.getAttribute("id") === "next-slide") {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+Reveal.addEventListener('slidechanged', function(event) {
+    if (!isPreviewFrame()) {
+        var sectionTag = Reveal.getCurrentSlide();
+        var videos = document.getElementsByTagName("video");
+        for (var i = 0; i < videos.length; i++) {
+            var eachVideo = videos[i];
+            if (eachVideo.parentNode === sectionTag) {
+                eachVideo.currentTime = 0.0;
+                eachVideo.play();
+            }
+        }
+    }
+});
